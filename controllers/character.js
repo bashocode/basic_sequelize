b6d4e7f9b4204ed3c7df7e1637f999dedf6a1ed6
Ikhda Muhammad Wildani <ikhdamuhammad@gmail.com>
2021-03-29 20:28:09 +0700

    get data by sequelize

--- controllers/character.js
+++ controllers/character.js
@@ -1,7 +1,38 @@
-class Character {
+const { Character, Skill } = require('../models');
+
+class CharacterController {
   async getCharacter(req, res, next) {
-    res.send('Halo my name is Diluc');
+    try {
+      let characters = await Character.findAll({
+        attributes: ['id', 'name'],
+        include: {
+          model: Skill,
+          attributes: ['element']
+        }
+      });
+      // convert data to string
+      let stringified = JSON.stringify(characters);
+      // convert string to object
+      let parsed = JSON.parse(stringified);
+      // lets mapp the data
+      let mapped = [];
+
+      // mapping the data
+      for (let i = 0; i < parsed.length; i++) {
+        let newData = {};
+        newData.id = parsed[i].id;
+        newData.name = parsed[i].name;
+        newData.element = parsed[i].Skill.element;
+        mapped.push(newData);
+      }
+
+      res.status(200).json({
+        result: mapped
+      });
+    } catch (error) {
+      res.status(400).json(error);
+    }
   }
 }
 
-module.exports = Character;
+module.exports = CharacterController;
