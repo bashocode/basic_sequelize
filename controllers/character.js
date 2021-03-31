4527ebf4902f69141b6634cfb5b67c8e02e3b5da
Mohammad Zul Fikri <fikriztm@gmail.com>
2021-03-31 13:24:50 +0800

    add update and delete

--- controllers/character.js
+++ controllers/character.js
@@ -82,6 +82,96 @@ class CharacterController {
       res.status(400).json(error);
     }
   }
+
+  async updateCharacter(req, res) {
+    try {
+      const { name, element_id } = req.body;
+    
+      if(name || element_id){
+        res.status(400).json({
+          // mas ikhda yang ngajarin
+          message: 'Woi mana nama dan elementnya ?.'
+        });
+      }
+
+      const findCharId = await Character.findOne({
+        where: {id: req.params.id}
+      })
+
+      if(findCharId == null){
+        res.status(400).json({
+          // mas ikhda yang ngajarin
+          message: 'Woi id karakter ini ga ada ?.'
+        });
+      }
+
+      const findElementId = await Skill.findOne({
+        where: {id : element_id}
+      })
+
+      if(findElementId == null){
+        res.status(400).json({
+          // mas ikhda yang ngajarin
+          message: 'Woi id element ini ga ada ?.'
+        });
+      }
+
+      const updatedCharacter = await Character.update({
+        name, element_id
+      },{where: {id: req.params.id}})
+
+    // convert data to string
+      let stringified = JSON.stringify(updatedCharacter);
+      // convert string to object
+      let parsed = JSON.parse(stringified);
+      let result = {};
+      result.id = parsed.id;
+      result.name = parsed.name;
+      result.element = parsed.Skill.element;
+      
+      res.status(201).json({
+        message: 'new Character successfully update(lupa kmaren kata katanya bagaimana yang ini)',
+        data: result
+      }); 
+    } catch (error) {
+      res.status(400).json(error);
+    }
+    
+  }
+
+  async deleteCharacter(req, res){
+    try {
+      const findCharId = await Character.findOne({
+        where: {id: req.params.id}
+      })
+
+      if(findCharId == null){
+        res.status(400).json({
+          // mas ikhda yang ngajarin
+          message: 'Woi id karakter ini ga ada ?.'
+        });
+      }
+      await Character.destroy({
+        where: {id : req.params.id}
+      })
+
+      // convert data to string
+      let stringified = JSON.stringify(findCharId);
+      // convert string to object
+      let parsed = JSON.parse(stringified);
+      let result = {};
+      result.id = parsed.id;
+      result.name = parsed.name;
+      result.element = parsed.Skill.element;
+      
+      res.status(201).json({
+        message: 'Character successfully deleted(lupa kmaren kata katanya bagaimana yang ini)',
+        data: findCharId
+      }); 
+    } catch (error) {
+      res.status(400).json(error); 
+    }
+  }
 }
 
 module.exports = CharacterController;
